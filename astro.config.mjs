103650d0d9e3371a040204a74ae9a0bd0bc6d106
Miguel Aréjula <arejula10@gmail.com>
2024-03-06 22:06:50 +0100

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://arejula11.github.io',
+    site: 'https://radiant-results.com/',
     base: '/radiant-results'
 });
