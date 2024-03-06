5b4fc3cd372a25f515eee167ac5d3a00c3223dd8
Miguel Aréjula <arejula10@gmail.com>
2024-03-06 23:33:53 +0100

    Update dependencies and configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://arejula11.github.io',
-    base: '/radiant-results'
+  site: 'https://arejula11.github.io',
+  base: '/radiant-results',
+  integrations: [tailwind()]
 });
---
+++ tailwind.config.mjs
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: "class",
+	theme: {
+		extend: {
+			colors: {
+				current: "currentColor",
+				primary:{
+					grey: "rgb(var(--color-primary-grey)) / <alpha-value>",
+					redRR: "#FF4655",
+				},
+				text: {
+					body: "rgb(var(--color-text-body)) / <alpha-value>",
+					bold: "rgb(var(--color-text-bold)) / <alpha-value>",
+				},
+				bg: {
+					body: "rgb(var(--color-bg-body)) / <alpha-value>",
+					
+				},
+				border: {
+					body: "rgb(var(--color-border-body)) / <alpha-value>",
+				},
+			}
+		},
+	},
+	plugins: [],
+}
